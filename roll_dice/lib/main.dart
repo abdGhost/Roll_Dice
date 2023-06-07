44ad2836257d567354d1787d83502a83f20963a9
abdul <ktaher642@gmail.com>
2023-06-07 16:23:48 +0530

    Working with GradientConatiner Class in Main.dart

--- roll_dice/lib/main.dart
+++ roll_dice/lib/main.dart
@@ -2,12 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Ghost"),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 88, 4, 96),
+            Color.fromARGB(255, 86, 2, 78),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Ghost",
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
 }
